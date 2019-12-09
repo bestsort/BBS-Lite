d3fd6174c7b2801b3f3ad2f46ede90f309c57e09
bestsort <bestsort126@gmail.com>
2019-12-09 15:00:48 +0800

    springboot admin test

--- src/main/java/cn/bestsort/bbslite/BBSLiteApplication.java
+++ src/main/java/cn/bestsort/bbslite/BBSLiteApplication.java
@@ -3,15 +3,16 @@ package cn.bestsort.bbslite;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author bestsort
+ */
 @MapperScan("cn.bestsort.bbslite.mapper")
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@EnableCaching
 public class BBSLiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(BBSLiteApplication.class, args);
